48c9f1d33a6892b358ff4961c25582b4f926a5a3
w6688j <953372680@qq.com>
2019-03-21 09:55:36 +0800

    web

--- KB_query/generate_sparql.py
+++ KB_query/generate_sparql.py
@@ -1,14 +1,6 @@
 from KB_query.questions_model import question2sparql
 
 
-# q2s = question2sparql.Question2Sparql([
-#     'external_dict/province_name.txt',
-#     'external_dict/time_name.txt',
-#     'external_dict/person_name.txt',
-#     'external_dict/movie_title.txt'])
-#
-# print(q2s.get_sparql('北京的人口数是多少?'))
-
 def generate_sparql(question):
     q2s = question2sparql.Question2Sparql([
         '../KB_query/external_dict/province_name.txt',
--- web/service.py
+++ web/service.py
@@ -1,19 +1,23 @@
 import json
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 from KB_query.generate_sparql import generate_sparql
 
 app = Flask(__name__)
 
 
+@app.route('/', methods=["GET"])
+def index():
+    return render_template('index.html')
+
+
 @app.route('/get_sparql', methods=["POST"])
 def get_sparql():
-    data = json.loads(request.get_data())
-    rs = generate_sparql(data['q'])
+    rs = generate_sparql(request.form.get('q'))
 
     return jsonify(code=200, message='ok', data={'sparql': rs})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
